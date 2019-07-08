f2b930f93af0f74bc12227d96f7a29cc5bf3b899
Simone Golinucci <simone.golinucci@studio.unibo.it>
2019-07-08 17:06:45 +0200

    Finita la prima versione del EventService e finito il modulo per semplificare l'interfacciamento con il servizio da parte dell'applicazione

--- app/src/EventService.js
+++ app/src/EventService.js
@@ -10,57 +10,214 @@ class EventService {
 
     }
 
-    getEvent(fun){
-        httpsClient.get(`${this.host}/event/id/${id}`, fun)
+    /**
+     * Ritorna gli eventi filtrati con i valori dell'oggetto query.
+     * 
+     * Codici di ritorno:
+     *  - 201: tutto bene risultati presenti
+     *  - 204: tutto bene ma risultati non esistenti
+     *  - 404: errore nella query
+     * 
+     * @param {Object} query dell'evento da selezionare.
+     * @param {Function} callback viene assegnata alla funzione request del modulo 'https' di nodejs.
+     */
+    getEvent(query = {}, callback = null){
+        let requestData = JSON.stringify(query)
+        httpsClient.get(`${this.hostport}/events`+ requestData, callback)
     }
 
-    getEventById(id,fun){
-        if(id){
-            httpsClient.get(`${this.host}/event/id/${id}`, fun)
+    /**
+     * Ritorna l'evento con id uuid.
+     * 
+     * Codici di ritorno:
+     *  - 201: tutto bene risultati presenti
+     *  - 204: tutto bene ma risultati non esistenti
+     *  - 404: errore nella query
+     * 
+     * @param {ObjectId} eventUuid dell'evento da selezionare.
+     * @param {Function} callback viene assegnata alla funzione request del modulo 'https' di nodejs.
+     */
+    getEventById(eventUuid, callback = null){
+        httpsClient.get(`${this.hostport}/events/${eventUuid}`, callback)
+    }
+
+    /**
+     * Ritorna tutte le reviews dell'evento identificato con uuid.
+     * 
+     * Codici di ritorno:
+     *  - 201: tutto bene risultati presenti
+     *  - 204: tutto bene ma risultati non esistenti
+     *  - 404: errore nella query
+     * 
+     * @param {ObjectId} eventUuid uuid dell'evento.
+     * @param {Function} callback viene assegnata alla funzione request del modulo 'https' di nodejs.
+     */
+    getEventReviews(eventUuid, callback = null){
+        httpsClient.get(`${this.hostport}/events/${eventUuid}/reviews`, callback)
+    }
+
+    /**
+     * Aggiunge una recensione all'evento
+     * Codici di ritorno:
+     * 
+     *  - 200: dato aggiunto correttamente
+     *  - 400: errore nell'inserimento
+     * 
+     * @param {ObjectId} eventUuid uuid dell'evento.
+     * @param {Function} callback viene assegnata alla funzione request del modulo 'https' di nodejs.
+     */
+    addEventReviews(eventUuid, callback = null){
+        data = JSON.stringify(dataToUpdate)
+        const option = {  
+            hostname: host,
+            port: port,
+            path: `/events/${eventUuid}/reviews`,
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': data.length
+            }
         }
+        let postRequest = httpsClient.request(option, callback)
+        postRequest.write(data)
+        postRequest.end()
     }
 
-    getEventByCreator(creator, fun){
-        if(creator){
-            httpsClient.get(`${this.host}/event/creator/${creator}`, fun)
+    /**
+     * Aggiorna l'evento con i dati presenti in dataToUpdate.
+     *
+     *  - 200: dato aggiunto correttamente
+     *  - 400: errore nell'inserimento
+     *
+     *  @param {ObjectId} eventUuid uuid dell'evento che si vuole assegnare l'aggiunta delle persone.
+     * @param {Object} dataToUpdate oggetto con dento i valori da assegnare per aggiornare l'evento. L'oggetto
+     * deve avere la seguiente struttura {user : {participants: value, followers: value}}.
+     * @param {Function} callback viene assegnata alla funzione request del modulo 'https' di nodejs.
+     */
+    updateEventById(eventUuid, dataToUpdate, callback = null){
+        data = JSON.stringify(dataToUpdate)
+        const option = {  
+            hostname: host,
+            port: port,
+            path: `/events/${eventUuid}`,
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': data.length
+            }
         }
+        let postRequest = httpsClient.request(option, callback)
+        postRequest.write(data)
+        postRequest.end()
     }
 
-    getEventByCreator(tipology, fun){
-        if(tipology){
-            httpsClient.get(`${this.host}/event/creator/${tipology}`, fun)
+    
+    /**
+     * Aggiunge gli utenti indicati nell'oggetto users all'evento indicato nella variabile eventUuid.
+     * 
+     *  - 200: dato aggiunto correttamente
+     *  - 400: errore nell'inserimento
+     * 
+     * @param {ObjectId} eventUuid uuid dell'evento che si vuole assegnare l'aggiunta delle persone.
+     * @param {Object} users oggetto con dento i valori da assegnare per aggiornare l'evento. L'oggetto
+     * deve avere la seguiente struttura {user : {participants: value, followers: value}}.
+     * @param {Function} callback viene assegnata alla funzione request del modulo 'https' di nodejs.
+     */
+    addUserToEvent(eventUuid, users, callback = null){
+        data = JSON.stringify(users)
+        const option = {  
+            hostname: host,
+            port: port,
+            path: `/events/${eventUuid}`,
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': data.length
+            }
         }
+        let postRequest = httpsClient.request(option, callback)
+        postRequest.write(data)
+        postRequest.end()
     }
 
-    createNewEvent(event, fun){
-        const option = {  hostname: host,
+    /**
+     * rimuove gli utenti indicati nell'oggetto users all'evento indicato nella variabile eventUuid.
+     * 
+     *  - 200: dato aggiunto correttamente
+     *  - 400: errore nell'inserimento
+     * 
+     * @param {ObjectId} eventUuid uuid dell'evento a cui si vogliono eliminare gli utenti
+     * @param {Object} users oggetto con dento i valori da assegnare per aggiornare l'evento. L'oggetto
+     * deve avere la seguiente struttura {user : {participants: value, followers: value}}.
+     * @param {Function} callback viene assegnata alla funzione request del modulo 'https' di nodejs.
+     */
+    removeUserToEvent(eventUuid, users, callback = null){
+        data = JSON.stringify(users)
+        const option = {  
+            hostname: host,
+            port: port,
+            path: `/events/${eventUuid}`,
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': data.length
+            }
+        }
+        let postRequest = httpsClient.request(option, callback)
+        postRequest.write(data)
+        postRequest.end()
+    }
+
+    /**
+     * Inserisce un nuovo evento del db.
+     * 
+     *  - 201: dato aggiunto correttamente
+     *  - 400: errore nell'inserimento
+     * 
+     * @param {Object} event da aggiungere nel db.
+     * @param {Function} callback viene assegnata alla funzione request del modulo 'https' di nodejs.
+     */
+    newEvent(event, callback){
+        data = JSON.stringify(event)
+        const option = {  
+            hostname: host,
             port: port,
             path: '/event/new',
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                'Content-Length': event.length
+                'Content-Length': data.length
             }
         }
         
-        let postRequest = httpsClient.request(option, fun);
-        postRequest.write(event);
+        let postRequest = httpsClient.request(option, callback)
+        postRequest.write(data)
+        postRequest.end()
     }
 }
 
 
-
-exports.createBasicEventJSON = (name, description, organizator, eventData, location, public, tipology, numberOfParticipant) => {
-    return `{
-        "event": {
-            "name": "${name}",
-            "description": "${description}",
-            "organizator": "${organizator}",
-            "eventData": "${eventData}",
-            "location": ${location},
-            "public": "${public}",
-            "tipology": ${tipology},
-            "maximumParticipant": ${numberOfParticipant}
-        }
-    }`
+/**
+ * per usare quest proprietà usare la seguente linea di codice:
+ * 
+ *  let event = Object.assign({}, moduleImported.Event);
+ * 
+ * questo permette di avere una copia correttamente modificabile dell'oggetto,
+ * lascianto quello origianle con i valori di default.
+ * 
+ * ! Trovare una soluzione più elegante
+ */
+exports.Event = {
+    name: '',
+    description: '',
+    organizator: '',
+    eventDate: Date.now,
+    location: {maps:'', coordinate:{lat: 0, long: 0}},
+    public: false,
+    tipology: {name:'', subtipology:''},
+    maximumParticipant:0,
+    participants: [],
+    followers:[],
+    thumbnail: '',
+    reviews: [],
 }
