b4845a5c3dab4e1727e696f66b3173373e53717a
Stefano Righini <stefano.righini6@studio.unibo.it>
2019-07-13 19:22:53 +0200

    Add Home page.

--- event-hub/src/components/event_card/EventCard.js
+++ event-hub/src/components/event_card/EventCard.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import Axios from 'axios';
 import './EventCard.css';
+import { Redirect } from 'react-router-dom'
 
 class EventCard extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            redirect: false
+        }
+    }
 
     participate = () => {
         this.interactWithEvent({participant: true, event: this.props.eventInfo._id}, "Errore durante la partecipazione ad un evento. Riprovare.")
@@ -13,22 +21,37 @@ class EventCard extends React.Component {
     }
 
     interactWithEvent = (message, errorMessage) => {
-        Axios.post("http://" + this.props.mainServer + "/events", message)
-            .then(response => {
-                let status = response.status
-                if (status !== 200) {
-                    this.props.onError(errorMessage)
-                }
-            })
+        if (this.props.isLogged)
+            Axios.post(this.props.mainServer + "/events", message)
+                .then(response => {
+                    let status = response.status
+                    if (status !== 200) {
+                        this.props.onError(errorMessage)
+                    }
+                })
+        else
+            this.setRedirect(true)
+    }
+
+    renderRedirect = () => {
+        if (this.state.redirect) {
+            this.setRedirect(false)
+            return <Redirect from='/' to='/profile' />
+        }
+    }
+
+    setRedirect = (value) => {
+        this.setState({redirect: value})
     }
 
     render() {
-        let type = this.props.event.typology
+        let type = this.props.eventInfo.typology
         return (
             <div className="row">
+                {this.renderRedirect()}
                 <div className="col-11 card shadow my-2 mx-auto px-0">
                     <div className="card bg-dark" id={this.props.eventInfo._id}>
-                        <img src={"http://" + this.props.mainServer + this.props.eventInfo.thumbnail} className="card-img" alt="locandina evento" />
+                        <img src={this.props.mainServer + this.props.eventInfo.thumbnail} className="card-img" alt="locandina evento" />
                         <div className="card-img-overlay text-white">
                             <div className="d-flex align-items-start flex-column h-100">
                                 <div className="container-fluid">
@@ -45,7 +68,7 @@ class EventCard extends React.Component {
                                 <div className="container-fluid mt-auto mb-2">
                                     <div className="row">
                                         <div className="col-12 card-text event-text">
-                                            <p className="m-0">{this.props.event.description}</p>
+                                            <p className="m-0">{this.props.eventInfo.description}</p>
                                         </div>
                                     </div>
                                 </div>
@@ -57,9 +80,11 @@ class EventCard extends React.Component {
                             <div className="col-3 my-auto">
                                 <div className={"badge badge-pill " + type + " " + type + "Badge"}>#{type.charAt(0).toUpperCase() + type.slice(1)}</div>
                             </div>
-                            <div className={(this.props.isLogged ? "" : "d-none ") + "col-9 d-flex justify-content-end"}>
-                                <button className={"btn " + type + "Button " + type + "ButtonSecondary"} onClick={this.participate}>Segui</button>
-                                <button className={"btn " + type + "Button " + type + "ButtonPrimary ml-2"} onClick={this.follow}>Partecipa</button>
+                            <div className={"col-9 "}>
+                                <div className="d-flex justify-content-end">
+                                    <button className={"btn " + type + "Button " + type + "ButtonSecondary"} onClick={this.participate}>Segui</button>
+                                    <button className={"btn " + type + "Button " + type + "ButtonPrimary ml-2"} onClick={this.follow}>Partecipa</button>
+                                </div>
                             </div>
                         </div>
                     </div>
--- event-hub/src/components/home/Home.js
+++ event-hub/src/components/home/Home.js
@@ -1,14 +1,56 @@
 import React from 'react';
 import './Home.css';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import EventCard from "../event_card/EventCard";
 
 class Home extends React.Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            isLogged: false,
-            showNavbar: true
+            eventsLoaded: [{
+                name: "Evento della madonna",
+                description: "Una madonna madonnesca",
+                typology: "sport",
+                thumbnail: "",
+                organizator: {
+                    name: "Pippo"
+                },
+                maxParticipants: 100,
+                numParticipants: 37
+            },
+            {
+                name: "Evento della madonna",
+                description: "Una madonna madonnesca",
+                typology: "incontro",
+                thumbnail: "",
+                organizator: {
+                    name: "Pippo"
+                },
+                maxParticipants: 100,
+                numParticipants: 37
+            },
+            {
+                name: "Evento della madonna",
+                description: "Una madonna madonnesca",
+                typology: "festa",
+                thumbnail: "",
+                organizator: {
+                    name: "Pippo"
+                },
+                maxParticipants: 100,
+                numParticipants: 37
+            },
+            {
+                name: "Evento della madonna",
+                description: "Una madonna madonnesca",
+                typology: "sport",
+                thumbnail: "",
+                organizator: {
+                    name: "Pippo"
+                },
+                maxParticipants: 100,
+                numParticipants: 37
+            }]
         }
     }
 
@@ -40,49 +82,18 @@ class Home extends React.Component {
                 </button>
 
                 <main className="main-container">
-                    <div className="row">
-                        <div className="col-11 card shadow my-2 mx-auto px-0">
-                            <div className="card bg-dark" id="locandina&evento">
-                                <img src="" className="card-img" alt="locandina evento" />
-                                    <div className="card-img-overlay text-white">
-                                        <div className="d-flex align-items-start flex-column h-100">
-                                            <div className="container-fluid">
-                                                <div className="row">
-                                                    <div className="col-8">
-                                                        <h5 className="card-title event-name">Evento della Madonna</h5>
-                                                        <h6 className="card-subtitle text-muted event-text">Organizzazione SPA</h6>
-                                                    </div>
-                                                    <div className="col-4 d-flex justify-content-end">
-                                                        <h5 className="card-title event-name">0000</h5>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div className="container-fluid mt-auto mb-2">
-                                                <div className="row">
-                                                    <div className="col-12 card-text event-text">
-                                                        Lorem ipsum dolor sit amet, soluta regione urbanitas vis in, qui
-                                                        elit populo ut.
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                            </div>
-                            <div className="card-body container-fluid py-2" id="bottoni&tag">
-                                <div className="row">
-                                    <div className="col-3 my-auto">
-                                        <div className="badge badge-pill festa festaBadge">#Festa</div>
-                                    </div>
-                                    <div className="col-9 d-flex justify-content-end">
-                                        <a href="#" className="btn festaButton festaButtonSecondary">Segui</a>
-                                        <a href="#" className="btn festaButton festaButtonPrimary ml-2">Partecipa</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    {
+                        this.state.eventsLoaded.map(event =>
+                            <EventCard mainServer={this.props.mainServer}
+                                       eventInfo={event}
+                                       onError={this.props.onError}
+                                       isLogged={this.props.isLogged}
+                            />)
+                    }
                 </main>
             </div>
         )
     }
 }
+
+export default Home
