00862b003b874d7dff8fe042dab014f071a219d0
Stefano Righini <stefano.righini6@studio.unibo.it>
2019-07-16 23:12:08 +0200

    Add notification page.

--- event-hub/src/components/app/App.js
+++ event-hub/src/components/app/App.js
@@ -5,11 +5,12 @@ import './App.css';
 import Home from "../home/Home";
 import Login from "../login/Login";
 import EventInfo from "../event_info/EventInfo";
+import Notifications from "../notifications/Notifications";
 
 /*
 
 <Route path="/profile" component={Profile} />
-<Route path="/notifications" component={Notifications} />
+<Route path="/notifications" component={Notification} />
 <Route path="/friends" component={Friends} />
 <Route path="/map" component={EventsMap} />
 
@@ -61,12 +62,20 @@ class App extends React.Component {
                 />} 
             />
 
+            <Route path="/notification" exact render={(props) =>
+                <Notifications {...props}
+                               mainServer={this.props.mainServer}
+                               imageFolderPath={this.props.imageFolderPath}
+                               onError={this.onError}
+                />}
+            />
+
             <footer className={(this.state.showNavbar ? "" : "d-none ") + "row fixed-bottom bg-light border-top border-primary mx-0 py-2"}>
                 <div className="col text-center my-auto"><Link to="/map"><em className="fas fa-map-marked-alt fa-lg" /></Link></div>
                 <div className="col text-center my-auto"><Link to={this.state.isLogged ? "/profile" : "/login"}><em className="fas fa-user fa-lg" /></Link></div>
                 <div className="col text-center my-auto"><Link to="/"><em className="fas fa-home fa-2x bg-primary text-white rounded-circle p-2" /></Link></div>
                 <div className="col text-center my-auto"><Link to="/friends"><em className="fas fa-users fa-lg" /></Link></div>
-                <div className="col text-center my-auto"><Link to="/notifications"><em className="fas fa-bell fa-lg" /></Link></div>
+                <div className="col text-center my-auto"><Link to="/notification"><em className="fas fa-bell fa-lg" /></Link></div>
             </footer>
         </Router>
     )
---
+++ event-hub/src/components/notifications/Notifications.js
@@ -0,0 +1,216 @@
+import React from 'react';
+import Axios from 'axios';
+import Notification from "./Notification";
+
+class Notifications extends React.Component {
+
+    constructor(props) {
+        super(props)
+        let timestamp = new Date().toString()
+        this.state = {
+            notifications: [
+                {
+                    _id: "0",
+                    typology: 0,
+                    sender: {
+                        _id: "2",
+                        name: "Stefano",
+                        surname: "Righini",
+                        avatar: "gatto.jpeg"
+                    },
+                    event: {
+                        name: "Evento della madonna",
+                        thumbnail: "party.jpg",
+                        _id: "3",
+                        tipology: "festa"
+                    },
+                    timestamp: timestamp
+                },
+                {
+                    _id: "1",
+                    typology: 1,
+                    sender: {
+                        _id: "2",
+                        name: "Stefano",
+                        surname: "Righini",
+                        avatar: "gatto.jpeg"
+                    },
+                    event: {
+                        name: "Evento della madonna",
+                        thumbnail: "party.jpg",
+                        _id: "3",
+                        tipology: "festa"
+                    },
+                    timestamp: timestamp
+                },
+                {
+                    _id: "2",
+                    typology: 2,
+                    sender: {
+                        _id: "2",
+                        name: "Stefano",
+                        surname: "Righini",
+                        avatar: "gatto.jpeg"
+                    },
+                    event: {
+                        name: "Evento della madonna",
+                        thumbnail: "party.jpg",
+                        _id: "3",
+                        tipology: "festa"
+                    },
+                    timestamp: timestamp
+                },
+                {
+                    _id: "3",
+                    typology: 3,
+                    sender: {
+                        _id: "2",
+                        name: "Stefano",
+                        surname: "Righini",
+                        avatar: "gatto.jpeg"
+                    },
+                    event: {
+                        name: "Evento della madonna",
+                        thumbnail: "party.jpg",
+                        _id: "3",
+                        tipology: "festa"
+                    },
+                    timestamp: timestamp
+                },
+                {
+                    _id: "4",
+                    typology: 4,
+                    sender: {
+                        _id: "2",
+                        name: "Stefano",
+                        surname: "Righini",
+                        avatar: "gatto.jpeg"
+                    },
+                    event: {
+                        name: "Evento della madonna",
+                        thumbnail: "party.jpg",
+                        _id: "3",
+                        tipology: "festa"
+                    },
+                    timestamp: timestamp
+                },
+                {
+                    _id: "5",
+                    typology: 5,
+                    sender: {
+                        _id: "2",
+                        name: "Stefano",
+                        surname: "Righini",
+                        avatar: "gatto.jpeg"
+                    },
+                    event: {
+                        name: "Evento della madonna",
+                        thumbnail: "party.jpg",
+                        _id: "3",
+                        tipology: "festa"
+                    },
+                    timestamp: timestamp
+                },
+                {
+                    _id: "6",
+                    typology: 6,
+                    sender: {
+                        _id: "2",
+                        name: "Stefano",
+                        surname: "Righini",
+                        avatar: "gatto.jpeg"
+                    },
+                    event: {
+                        name: "Evento della madonna",
+                        thumbnail: "party.jpg",
+                        _id: "3",
+                        tipology: "festa"
+                    },
+                    timestamp: timestamp
+                },
+                {
+                    _id: "7",
+                    typology: 7,
+                    sender: {
+                        _id: "2",
+                        name: "Stefano",
+                        surname: "Righini",
+                        avatar: "gatto.jpeg"
+                    },
+                    event: {
+                        name: "Evento della madonna",
+                        thumbnail: "party.jpg",
+                        _id: "3",
+                        tipology: "festa"
+                    },
+                    timestamp: timestamp
+                },
+                {
+                    _id: "8",
+                    typology: 8,
+                    sender: {
+                        _id: "2",
+                        name: "Stefano",
+                        surname: "Righini",
+                        avatar: "gatto.jpeg"
+                    },
+                    event: {
+                        name: "Evento della madonna",
+                        thumbnail: "party.jpg",
+                        _id: "3",
+                        tipology: "festa"
+                    },
+                    timestamp: timestamp
+                },
+                {
+                    _id: "9",
+                    typology: 9,
+                    sender: {
+                        _id: "2",
+                        name: "Stefano",
+                        surname: "Righini",
+                        avatar: "gatto.jpeg"
+                    },
+                    event: {
+                        name: "Evento della madonna",
+                        thumbnail: "party.jpg",
+                        _id: "3",
+                        tipology: "festa"
+                    },
+                    timestamp: timestamp
+                }
+            ]
+        }
+        Axios.get(this.props.mainServer + "/notifications/" + this.props.match.params.fromIndex)
+            .then(response => {
+                let status = response.status
+                if (status !== 200)
+                    this.props.onError("Errore durante il caricamento dei dati di un evento.")
+                else
+                    this.setState({ notifications: response })
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                <section className="row sticky-top shadow bg-white border-bottom border-primary text-center">
+                    <h1 className="col ml-1">Notifiche</h1>
+                </section>
+
+                <main className="main-container">
+                    {
+                        this.state.notifications.map(notification =>
+                            <Notification key={notification._id}
+                                          mainServer={this.props.mainServer}
+                                          imageFolderPath="../../assets/images"
+                                          notification={notification}
+                            />)
+                    }
+                </main>
+            </div>
+        )
+    }
+}
+
+export default Notifications
