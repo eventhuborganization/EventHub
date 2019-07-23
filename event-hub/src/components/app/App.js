a22a9def64eba305b76fb05a81623856875ddc5f
Francesco Grandinetti <francesco.grandinett@gmail.com>
2019-07-23 16:12:26 +0200

    add automatic scroll to up to all pages

--- event-hub/src/components/app/App.js
+++ event-hub/src/components/app/App.js
@@ -11,6 +11,7 @@ import Notifications from '../notifications/Notifications'
 import EventCreator from '../event_creator/EventCreator'
 import UserProfile from '../profile/UserProfile'
 import { CallableComponent } from '../redirect/Redirect'
+import ScrollToTop from '../scroll_to_top/ScrollToTop'
 
 
 class App extends React.Component {
@@ -54,86 +55,87 @@ class App extends React.Component {
   render() {
     return (
         <Router>
-            <ErrorModal
-              onRef={this.errorElement}
-              title="Errore"
-              closeLabel="Chiudi"
-            /> 
-            <Switch>
-              <Route path="/" exact render={(props) => 
-                  <Home {...props} 
-                    isLogged={this.state.isLogged} 
-                    mainServer={this.props.mainServer} 
-                    onError={this.onError} 
-                  />} 
-              />
-              <Route path="/event/new" exact render={(props) =>
-                  <EventCreator {...props}
-                                isLogged={this.state.isLogged}
-                                mainServer={this.props.mainServer}
-                                onError={this.onError}
-                                loggedUser={{
-                                    name: "Stefano",
-                                    surname: "Righini",
-                                    email: "rigo96.imola@gmail.com",
-                                    phoneNumber: "+39 3472284853",
-                                    avatar: "user-profile-image.jpg"
-                                }/*this.state.user*/}
-                  />}
-              />
-              <Route path="/event/:id" exact render={(props) => 
-                  <EventInfo {...props} 
-                    isLogged={this.state.isLogged} 
-                    mainServer={this.props.mainServer} 
-                    onError={this.onError} 
-                  />} 
-              />
-              <Route path="/login" exact render={(props) => 
-                  <Login {...props} 
-                    mainServer={this.props.mainServer} 
-                    onError={this.onError} 
-                    onLoginSuccessfull={this.onLoginSuccessfull} 
-                  />} 
-              />
-              <Route path="/register" exact render={(props) => 
-                  <Registration {...props} 
-                    mainServer={this.props.mainServer} 
-                    onError={this.onError} 
-                    onRegistration={this.onRegistrationSuccessfull} 
-                  />} 
-              />
-              <Route path="/notification" exact render={(props) =>
-                  <Notifications {...props}
-                      mainServer={this.props.mainServer}
-                      isLogged={this.state.isLogged}
-                      onError={this.onError}
-                  />}
-              />
-              <Route path="/profile" exact render={(props) =>
-                  <UserProfile {...props}
-                      mainServer={this.props.mainServer}
-                      userId={this.state.userId}
-                      onError={this.onError}
-                      isLocalUser={true}
-                  />}
-              />
-              <Route path="/users/:id" exact render={(props) => 
-                  <UserProfile {...props}
-                      mainServer={this.props.mainServer}
-                      userId={this.state.userId}
-                      onError={this.onError}
-                      isLocalUser={false}
-                  />} 
-              />
-            </Switch>
+          <ScrollToTop />
+          <ErrorModal
+            onRef={this.errorElement}
+            title="Errore"
+            closeLabel="Chiudi"
+          /> 
+          <Switch>
+            <Route path="/" exact render={(props) => 
+                <Home {...props} 
+                  isLogged={this.state.isLogged} 
+                  mainServer={this.props.mainServer} 
+                  onError={this.onError} 
+                />} 
+            />
+            <Route path="/event/new" exact render={(props) =>
+                <EventCreator {...props}
+                              isLogged={this.state.isLogged}
+                              mainServer={this.props.mainServer}
+                              onError={this.onError}
+                              loggedUser={{
+                                  name: "Stefano",
+                                  surname: "Righini",
+                                  email: "rigo96.imola@gmail.com",
+                                  phoneNumber: "+39 3472284853",
+                                  avatar: "user-profile-image.jpg"
+                              }/*this.state.user*/}
+                />}
+            />
+            <Route path="/event/:id" exact render={(props) => 
+                <EventInfo {...props} 
+                  isLogged={this.state.isLogged} 
+                  mainServer={this.props.mainServer} 
+                  onError={this.onError} 
+                />} 
+            />
+            <Route path="/login" exact render={(props) => 
+                <Login {...props} 
+                  mainServer={this.props.mainServer} 
+                  onError={this.onError} 
+                  onLoginSuccessfull={this.onLoginSuccessfull} 
+                />} 
+            />
+            <Route path="/register" exact render={(props) => 
+                <Registration {...props} 
+                  mainServer={this.props.mainServer} 
+                  onError={this.onError} 
+                  onRegistration={this.onRegistrationSuccessfull} 
+                />} 
+            />
+            <Route path="/notification" exact render={(props) =>
+                <Notifications {...props}
+                    mainServer={this.props.mainServer}
+                    isLogged={this.state.isLogged}
+                    onError={this.onError}
+                />}
+            />
+            <Route path="/profile" exact render={(props) =>
+                <UserProfile {...props}
+                    mainServer={this.props.mainServer}
+                    userId={this.state.userId}
+                    onError={this.onError}
+                    isLocalUser={true}
+                />}
+            />
+            <Route path="/users/:id" exact render={(props) => 
+                <UserProfile {...props}
+                    mainServer={this.props.mainServer}
+                    userId={this.state.userId}
+                    onError={this.onError}
+                    isLocalUser={false}
+                />} 
+            />
+          </Switch>
 
-            <footer className={(this.state.showNavbar ? "" : "d-none ") + "row fixed-bottom bg-light border-top border-primary mx-0 py-2"}>
-                <div className="col text-center my-auto"><Link to="/map"><em className="fas fa-map-marked-alt fa-lg" /></Link></div>
-                <div className="col text-center my-auto"><Link to={this.state.isLogged ? "/profile" : "/login"}><em className="fas fa-user fa-lg" /></Link></div>
-                <div className="col text-center my-auto"><Link to="/"><em className="fas fa-home fa-2x bg-primary text-white rounded-circle p-2" /></Link></div>
-                <div className="col text-center my-auto"><Link to="/friends"><em className="fas fa-users fa-lg" /></Link></div>
-                <div className="col text-center my-auto"><Link to="/notification"><em className="fas fa-bell fa-lg" /></Link></div>
-            </footer>
+          <footer className={(this.state.showNavbar ? "" : "d-none ") + "row fixed-bottom bg-light border-top border-primary mx-0 py-2"}>
+              <div className="col text-center my-auto"><Link to="/map"><em className="fas fa-map-marked-alt fa-lg" /></Link></div>
+              <div className="col text-center my-auto"><Link to={this.state.isLogged ? "/profile" : "/login"}><em className="fas fa-user fa-lg" /></Link></div>
+              <div className="col text-center my-auto"><Link to="/"><em className="fas fa-home fa-2x bg-primary text-white rounded-circle p-2" /></Link></div>
+              <div className="col text-center my-auto"><Link to="/friends"><em className="fas fa-users fa-lg" /></Link></div>
+              <div className="col text-center my-auto"><Link to="/notification"><em className="fas fa-bell fa-lg" /></Link></div>
+          </footer>
         </Router>
     )
   }
