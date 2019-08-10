8b24e1ce4712eec5a7aa588706961f7c882e468e
Francesco Grandinetti <francesco.grandinett@gmail.com>
2019-08-10 15:43:23 +0200

    fix a bug in web-server's routes with unordered routes

--- web-server/app/src/routes/routes.js
+++ web-server/app/src/routes/routes.js
@@ -7,57 +7,22 @@ module.exports = (app) => {
     app.route("/")
         .get(sessionChecker,(req, res) => {
             res.sendFile(`${appRoot}/views/home.html`);
-        });
-    
-        app.route('/images/:name')
-    .get((req, res) => {
-        res.sendFile(`${appRoot}/public/images/events/${req.params.name}`);
-    })
+        })
+
+    app.route('/images/:name')
+        .get((req, res) => {
+            res.sendFile(`${appRoot}/public/images/events/${req.params.name}`);
+        })
 
     app.route('/avatars/:name')
-    .get((req, res) => {
-        res.sendFile(`${appRoot}/public/images/users/${req.params.name}`);
-    })
+        .get((req, res) => {
+            res.sendFile(`${appRoot}/public/images/users/${req.params.name}`);
+        })
 
-
-    app.route("/events/:fromIndex")
-        .get(eventController.getEventsFromIndex)
-
-    app.route("/notification/friendship")
-        .post(sessionChecker,userController.friendshipAnswer);
-
-    app.route("/notification/friendposition")
-        .post(sessionChecker, userController.responseFriendPosition);
-
-    app.route("/registration")
-        .post(loginController.registration);
-
-    app.route("/profile")
-        .put(sessionChecker, userController.updateProfile);
+    /* ----------------------------------------------- */
 
     app.route("/events")
-        .put(sessionChecker, eventController.createEvent);
-
-    app.route("/profile/credentiels")
-        .put(userController.updateCredentials);
-
-    app.route("/users/:uuid")
-        .get(userController.getInfoUser);
-
-    app.route("/users/search/:name")
-        .get(userController.searchUser);
-
-// *-------------------------------------------------------------------------
-
-    app.route('/login')
-        .get(loginController.getLogin)
-        .post(loginController.login)
-        
-    app.route('/logout')
-        .get(sessionChecker, loginController.logout)
-
-    app.route('/user/event')
-        .post(sessionChecker, eventController.addUserToEvent)
+        .put(sessionChecker, eventController.createEvent)
 
     app.route('/events/info/:uuid')
         .get(sessionChecker, eventController.eventInfo)
@@ -68,20 +33,57 @@ module.exports = (app) => {
     app.route('/events/search/:name')
         .get(sessionChecker, eventController.searchEventByName)
 
+    app.route("/events/:fromIndex")
+        .get(eventController.getEventsFromIndex)
+
+    app.route('/user/event')
+        .post(sessionChecker, eventController.addUserToEvent)
+
+    /* ----------------------------------------------- */
+
+    app.route("/users/search/:name")
+        .get(userController.searchUser)
+    
+    app.route('/users/friendposition')
+        .post(sessionChecker, userController.requestFriendPosition)
+
     app.route('/users/friendship')
         .post(sessionChecker, userController.userFriendRequest)
         .delete(sessionChecker, userController.removeLinkedUser)
+
+    app.route("/users/:uuid")
+        .get(userController.getInfoUser)
 
     app.route('/notifications')
         .post(sessionChecker, notificationController.markNotificationAsReaded)
     
     app.route('/notifications/:fromIndex')
         .post(sessionChecker, notificationController.getNotification)
-    
-    app.route('/users/friendposition')
-        .post(sessionChecker, userController.requestFriendPosition);
-        
+
+    app.route("/notification/friendship")
+        .post(sessionChecker,userController.friendshipAnswer)
+
+    app.route("/notification/friendposition")
+        .post(sessionChecker, userController.responseFriendPosition)
+
+    app.route("/profile")
+        .put(sessionChecker, userController.updateProfile)
+
+    app.route("/profile/credentials")
+        .put(userController.updateCredentials)
+
     app.route('/invite/:uuid')
         .get(sessionChecker, userController.inviteFriends)
 
+    /* ----------------------------------------------- */
+
+    app.route("/registration")
+        .post(loginController.registration)
+
+    app.route('/login')
+        .get(loginController.getLogin)
+        .post(loginController.login)
+        
+    app.route('/logout')
+        .get(sessionChecker, loginController.logout)
 }
