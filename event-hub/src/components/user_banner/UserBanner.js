eb709f97e9e09af98065d16bfe5ea60a8e7ad676
Francesco Grandinetti <francesco.grandinett@gmail.com>
2019-08-11 16:50:01 +0200

    fix a bug that occurs when a user doesn't have a profile picture

--- event-hub/src/components/user_banner/UserBanner.js
+++ event-hub/src/components/user_banner/UserBanner.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-let images = require.context("../../assets/images", true)
+import Api from '../../services/api/Api'
 
 
 export default function UserBanner(props) {
     return (
         <div className={"row py-2 d-flex align-items-center" + (!!props.border ? " border-bottom" : "")}>
-            <Link to={"/users/" + props.user._id} className="col-3 col-md-1">
-                <img src={(props.user.avatar ? images(`./${props.user.avatar}`) : '')}
-                    className="img-fluid border rounded-circle"
-                    alt="Immagine profilo utente"
-                />
+            <Link to={"/users/" + props.user._id} className={"col-3 col-md-2 col-lg-1" + (props.user.avatar ? "" : " d-flex align-self-stretch")}>
+                {
+                    props.user.avatar ?
+                        <img src={Api.getAvatarUrl(props.user.avatar)}
+                            className="img-fluid border rounded-circle"
+                            alt="Immagine profilo utente"
+                        /> :
+                        <div className="text-secondary border border-primary rounded-circle w-100 h-100 d-flex justify-content-center align-items-center">
+                            <em className="far fa-image fa-2x"></em>
+                        </div>
+                }
             </Link>
             <Link to={"/users/" + props.user._id} className="col-6 px-0" style={{textDecoration: "none"}}>
                 <div className="font-weight-bold text-dark">{props.user.name + (props.user.organization ? "" : " " + props.user.surname)}</div>
