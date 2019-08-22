0578f166c3c8939f87361134e23cd8efdcc8bf70
Francesco Grandinetti <francesco.grandinett@gmail.com>
2019-08-22 14:43:21 +0200

    change Contacts in order to not show the phone when it isn't set

--- web-app/app/src/components/contacts/Contacts.js
+++ web-app/app/src/components/contacts/Contacts.js
@@ -1,30 +1,31 @@
 import React from "react";
 
 let Contacts = props => {
-    //if (props.event && props.event.organizator && props.event.organizator.phoneNumber && props.event.organizator.email)
-        return (
-            <section className="row mt-2">
-                <div className="col-12">
-                    <h5>Contatti</h5>
-                    <div className="container-fluid px-1">
+    return props.event && props.event.organizator ? (
+        <section className="row mt-2">
+            <div className="col-12">
+                <h5>Contatti</h5>
+                <div className="container-fluid px-1">
+                    {
+                        props.event.organizator.phone ? 
                         <div className="row">
                             <div className="col-2 d-flex align-items-center justify-content-center">
                                 <em className="fas fa-phone fa-2x text-secondary"></em>
                             </div>
                             <p className="col my-0 d-flex align-items-center">{props.event.organizator.phone}</p>
                         </div>
-                        <div className="row">
-                            <div className="col-2 d-flex align-items-center justify-content-center">
-                                <em className="fas fa-envelope fa-2x rounded text-secondary"></em>
-                            </div>
-                            <p className="col my-0 d-flex align-items-center">{props.event.organizator.email}</p>
+                        : <div/>
+                    }
+                    <div className="row">
+                        <div className="col-2 d-flex align-items-center justify-content-center">
+                            <em className="fas fa-envelope fa-2x rounded text-secondary"></em>
                         </div>
+                        <p className="col my-0 d-flex align-items-center">{props.event.organizator.email}</p>
                     </div>
                 </div>
-            </section>
-        )
-    /*else
-        return (<div />)*/
+            </div>
+        </section>
+    ) : <div />
 }
 
 export default Contacts
