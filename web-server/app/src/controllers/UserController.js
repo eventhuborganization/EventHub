c89cd1865ab2e821d8b9fa924bbcefdfdfab5570
Francesco Grandinetti <francesco.grandinett@gmail.com>
2019-09-05 17:27:36 +0200

    fix a bug in the backend with groups invite

--- web-server/app/src/controllers/UserController.js
+++ web-server/app/src/controllers/UserController.js
@@ -19,16 +19,16 @@ exports.inviteFriends = (req, res) => {
     }
     if(req.body.group){
         option.group = req.body.group
-        inviteGroup(res,option)
-    }else if (req.body.user){
+        inviteGroup(req, res, option)
+    } else if (req.body.user){
         option.user = req.body.user
-        inviteUser(res,option)
+        inviteUser(req, res, option)
     } else {
         network.badRequest(res)
     }
 }
 
-let inviteGroup = (res, option) => {
+let inviteGroup = (req, res, option) => {
     UserService.getGroupInfo(option.group)
     .then(response => {
         let userPromise = response.data.members.map(m => {
@@ -36,27 +36,21 @@ let inviteGroup = (res, option) => {
             return UserService.sendNotification(m, data)
         })
         Promise.all(userPromise)
-        .then((response) => {
+            .then(() =>  network.result(res))
+            .catch((err) => network.replayError(err, res))
+    })
+    .catch((err) => network.internalError(res, err))
+}
+
+let inviteUser = (req, res, option) => {
+    var data = {typology: 0, sender: req.user._id, data: {eventId: option.event}}
+    UserService.sendNotification(option.user, data)
+        .then(() => {
             network.result(res)
         })
         .catch((err) => {
             network.internalError(res, err)
         })
-    })    
-    .catch((err) => {
-        network.internalError(res, err)
-    })
-}
-
-let inviteUser = (res, option) => {
-    var data = {typology: 0, sender: req.user._id, data: {eventId: option.event}}
-    UserService.sendNotification(option.user, data)
-    .then((response) => {
-        network.result(res)
-    })
-    .catch((err) => {
-        network.internalError(res, err)
-    })
 }
 
 exports.addFollower = (req, res) => {
@@ -64,13 +58,8 @@ exports.addFollower = (req, res) => {
         .then(() => {
             return  UserService.sendNotification(req.body.uuid, {typology: 2, sender: req.user._id})
         })
-        .then(response => {
-            network.replayResponse(response, res);
-        })
-        .catch (error => {
-            console.log(error)
-            network.internalError(res, error);
-        })
+        .then(response => network.replayResponse(response, res))
+        .catch (error => network.internalError(res, error))
 }
 
 exports.userFriendRequest = (req, res) => {
