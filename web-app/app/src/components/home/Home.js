395140e712ebb55090dda5927eeddc4b01331d53
Stefano Righini <stefano.righini6@studio.unibo.it>
2019-09-08 16:09:06 +0200

    Fix event cards display for tablet and desktop in the home page.

--- web-app/app/src/components/home/Home.js
+++ web-app/app/src/components/home/Home.js
@@ -40,11 +40,19 @@ class Home extends React.Component {
 
     renderEvents = () => {
         if (this.state.displayEvents && this.state.eventsLoaded.length > 0) {
-            return this.state.eventsLoaded.map(event =>
-                <EventCard {...this.props}
-                           key={event._id}
-                           eventInfo={event}
-                />)
+            return (
+                <div className={"row"}>
+                    {
+                        this.state.eventsLoaded.map(event =>
+                            <div className={"col-12 col-md-6 col-xl-3"}>
+                                <EventCard {...this.props}
+                                           key={event._id}
+                                           eventInfo={event}
+                                />
+                            </div>)
+                    }
+                </div>
+            )
         } else if (this.state.displayEvents) {
             return <LoadingSpinner/>
         } else {
